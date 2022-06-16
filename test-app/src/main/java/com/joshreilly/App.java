01143b2dcd68a868e89b07cae31a3396dd321901
JoshReilly <joshreilly2000@gmail.com>
2022-06-16 13:16:19 +0100

    Added      test-app/pom.xml Added      test-app/src/main/java/com/joshreilly/App.java Added      test-app/src/test/java/com/joshreilly/AppTest.java

---
+++ test-app/src/main/java/com/joshreilly/App.java
@@ -0,0 +1,13 @@
+package com.joshreilly;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
